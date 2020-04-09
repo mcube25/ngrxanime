c2c36f53e9dc02406c9aa6c935ae21955d0526a2
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-04-09 12:31:17 -0700

    using animations callback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state , style, transition, animate} from '@angular/core';
+import { trigger, state , style, transition, animate, keyframes} from '@angular/core';
 
 
 @Component({
@@ -63,6 +63,47 @@ transition('normal <=> highlighted', animate(300)),
         ]),
         //transition(' highlighted => normal ', animate(800)),
           ]),
+          trigger('list2', [
+            state('in', style({
+              opacity : 1,
+              transform: 'translateX(0)'
+            })),
+            transition('void => *',[
+              animate(1000, keyframes([
+                style({
+                  transform: 'translateX(-100px)',
+                   opacity: 0,
+                   offset: 0
+                }),
+                style({
+                transform: 'translateX(-50px)',
+                 opacity: 0.5,
+                  offset: 0.3
+               }),
+               style({
+               transform: 'translateX(-20px)',
+                opacity: 1,
+                 offset: 1
+              }),
+              style({
+              transform: 'translateX(0px)',
+               opacity: 1,
+                offset: 0.8
+             })
+              ]))
+            ]),
+            transition('* => void',[
+              group([
+                animate(300, style({
+                color: 'red'
+              })),
+                animate(300, style({
+                transform: 'translateX(100px)'  ,
+                 opacity: 0
+               }))
+              ])
+            ])
+            ]),
   ]
 })
 export class AppComponent {
@@ -84,5 +125,11 @@ onDelete(item){
 }
     onAdd(item) {
       this.list.push(item);
+    }
+    animationStarted(event){
+      console.log(event);
+    }
+    animationEnded(event){
+    console.log(event) ; 
     }
 }
